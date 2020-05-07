9f2e0d774988a966e627c9786ca892c5eb083385
Kārlis Ozoliņš <52657221+ZeKarlish@users.noreply.github.com>
2020-05-07 20:56:38 +0300

    LD3 done

--- LD1/LD1.cpp
+++ LD1/LD1.cpp
@@ -2,8 +2,11 @@
 #include <string>
 #include <iostream>
 
-using namespace System;
+
 using namespace std;
+
+class OverflowExc { // var jau sseit pabeigt ar };
+};
 
 class Animal {
 protected:
@@ -16,10 +19,10 @@ public:
 	virtual ~Animal()
 	{
 		cout << "Object with values: ";
-		Animal::print();
+		Animal::Print();
 		cout << " destroyed!" << endl;
 	}
-	virtual void print() const;
+	virtual void Print() const;
 	string GetName() const;
 	string GetNickname() const;
 	short GetBirthYear() const;
@@ -45,14 +48,14 @@ public:
 	virtual ~ZooAnimal()
 	{
 		cout << "Object with values: ";
-		ZooAnimal::print();
+		ZooAnimal::Print();
 		cout << " destroyed!" << endl;
 	}
 
-	virtual void print() const
+	virtual void Print() const
 	{
-		Animal::print();
-		cout << "Sector = " << sector;
+		Animal::Print();
+		cout << " Sector = " << sector;
 	}
 
 	int get_sector() const
@@ -65,6 +68,67 @@ public:
 	}
 
 };
+
+class Zoo
+{
+private:
+	typedef ZooAnimal* ZAnimal;
+	ZAnimal* Animals;
+	static const unsigned int DEFAULT_MAX_LENGTH = 5;
+	unsigned int MaxLength;
+	unsigned int Length;
+public:
+	Zoo() : MaxLength(DEFAULT_MAX_LENGTH), Length(0) {
+		Animals = new ZAnimal[MaxLength];
+	}
+	Zoo(unsigned int MaxLength) : MaxLength(MaxLength), Length(0) {
+		Animals = new ZAnimal[MaxLength];
+	}
+	~Zoo() {
+		for (unsigned int i = 0; i < Length; i++)
+			delete Animals[i];
+		delete[] Animals;
+	};
+	static unsigned int GetDefaultMaxLength() {
+		return DEFAULT_MAX_LENGTH;
+	}
+	int GetMaxLength() const {
+		return MaxLength;
+	}
+	int GetLength() const {
+		return Length;
+	}
+
+	void Add(const ZooAnimal& animal)
+	{
+		if (Length == MaxLength)
+			throw OverflowExc(); // throw and return
+		Animals[Length++] = new ZooAnimal(
+			animal.GetName(), animal.GetNickname(), animal.GetBirthYear(), animal.get_sector());
+	};
+
+	void  Print() const {
+		cout << endl << "Line's nodes:" << endl;
+		for (unsigned int i = 0; i < Length; i++) {
+			cout << (i + 1) << ". ";
+			Animals[i]->Print();
+			cout << "." << endl;
+		}
+	};
+
+	short GetMinBirth() const
+	{
+		short min = Animals[0]->GetBirthYear();
+		for (unsigned int i = 1; i < Length; i++) {
+			if (Animals[i]->GetBirthYear() < min)
+			{
+				min = Animals[i]->GetBirthYear();
+			}
+		}
+		return min;
+	};
+};
+
 
 Animal::Animal() : name("name"), nickname("Nickname"), birthYear(2020)
 {}
@@ -90,9 +154,9 @@ inline string Animal::GetNickname() const
 	return nickname;
 }
 
-inline void Animal::print() const
+inline void Animal::Print() const
 {
-	cout << "Name: " << name << " Nickname: " << nickname << " Birth Year: " << birthYear ;
+	cout << "Name: " << name << " Nickname: " << nickname << " Birth Year: " << birthYear;
 }
 
 inline void Animal::SetName(std::string n)
@@ -110,42 +174,76 @@ inline void Animal::SetNickname(string nick)
 	nickname = nick;
 }
 
+
+
+
+
+
+
 int main(void)
 {
-	const int N = 3;
 
-	Animal* animals[N ]
+	cout << "\n\nDefault maximal length (from CLASS): " <<
+		Zoo::GetDefaultMaxLength() << "." << endl;
+	
+	Zoo *zoo = new Zoo(2);
+	ZooAnimal *a_animal = new ZooAnimal("AAA", "A", 100, 5);
+	ZooAnimal b_animal("BBB", "B", 10, 6);
+	ZooAnimal c_animal("CCC", "C", 5, 4);
+	ZooAnimal d_animal("DDD", "D", 54, 3);
+	ZooAnimal e_animal("EEE", "E", 20, 2);
+
+
+	try
 	{
-		new Animal(),
-		new ZooAnimal("Horse","H", 325, 5),
-		new ZooAnimal("Volfy","V", 2010, 1)
-	};
-	
-	cout << "Array of animals: " << endl;
-	
-	for (int i = 0; i < N; i++) {
-		cout << (i + 1) << ". ";
-		animals[i]->print();
-		cout << endl;
+		zoo->Add(*a_animal);
+		std::cout << "a_animal added" << endl;
 	}
-	cout << endl << endl;
-	for (int i = 0; i < N; i++) {
-		delete animals[i];
+	catch (const OverflowExc)
+	{
+		cout << "*** Error: maximal length exceeded ! ***" << endl;
+	}
+	catch (...) {
+		cout << "Unknown Error !" << endl;
 	}
 	
-	//
-	// Animal* A2;
-	//
-	//
-	// A2 = new Animal("Beep", "bop", 2020);
-
-	// A2->SetName("NewName");
-	//
-	// (*A2).print();
+	delete a_animal;
 
 
+	cout << "Maximal length: " << zoo->GetMaxLength() << "." << endl;
+	cout << "Current length: " << zoo->GetLength() << "." << endl;
 
+
+	try
+	{
+		zoo->Add(b_animal);
+		std::cout << "a_animal added" << endl;
+	}
+	catch (const OverflowExc)
+	{
+		cout << "*** Error: maximal length exceeded ! ***" << endl;
+	}
+	catch (...) {
+		cout << "Unknown Error !" << endl;
+	}
+	cout << "Current length: " << zoo->GetLength() << "." << endl;
+
+	try
+	{
+		zoo->Add(b_animal);
+		std::cout << "a_animal added" << endl;
+	}
+	catch (const OverflowExc)
+	{
+		cout << "*** Error: maximal length exceeded ! ***" << endl;
+	}
+	catch (...) {
+		cout << "Unknown Error !" << endl;
+	}
+	cout << "Current length: " << zoo->GetLength() << "." << endl;
+
+	cout << "Min birth year:" << zoo->GetMinBirth();
+	
 	cout << "\n\n";
-
 	return 0;
 }
